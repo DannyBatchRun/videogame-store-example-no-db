48e4087171395bb9fbe04f265cf948c1e4466422
Daniele <116575866+VintageMachine@users.noreply.github.com>
2024-02-06 16:05:04 +0100

    Added homepage and health check to VideogameStore products

--- store-videogame-products-example/src/main/java/com/videogame/example/second/controller/SubscriptionController.java
+++ store-videogame-products-example/src/main/java/com/videogame/example/second/controller/SubscriptionController.java
@@ -1,5 +1,7 @@
 package com.videogame.example.second.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,15 +9,42 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
 
 import com.videogame.example.second.model.Videogame;
 
 import java.util.*;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class SubscriptionController {
 	
 	private List<Videogame> videogamesList = new ArrayList<>();
+
+	@GetMapping("/") 
+	public ResponseEntity<String> getHome() {
+		String welcome = "Welcome to VideoGame Store! Please add videogame to Database to path /add/videogame.\n" +
+						 "Otherwise, please delete with /remove/{idProduct} <--- Instead of idProduct, select the id you want to delete.";
+					return new ResponseEntity<>(welcome, HttpStatus.OK);
+	}
+
+	@GetMapping("/health")
+    	public ResponseEntity<String> checkStatus(HttpServletRequest request) {
+	        RestTemplate restTemplate = new RestTemplate();
+	        String url = request.getRequestURL().toString();
+	        try {
+	            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+	            if (response.getStatusCodeValue() == 200) {
+	                return new ResponseEntity<>("Service is up", HttpStatus.OK);
+	            } else {
+	                return new ResponseEntity<>("Service is down", HttpStatus.SERVICE_UNAVAILABLE);
+	            }
+	        } catch (HttpStatusCodeException ex) {
+	            return new ResponseEntity<>("Error occurred", HttpStatus.SERVICE_UNAVAILABLE);
+	        }
+    	}
 	
 	@GetMapping("/videogames") 
 	public List<Videogame> getVideogames() {
