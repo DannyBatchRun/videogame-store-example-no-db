8ad5748fd2ddb07ad66c37be765dd4892f448cc4
VintageMachine <danielpeter.mnz@gmail.com>
2024-02-15 15:42:49 +0100

    Added jenkins pipeline libraries for Videogame

---
+++ jenkins-libraries-example/src/main/groovy/com/libraries/jenkins/JenkinsApplication.groovy
@@ -0,0 +1,13 @@
+package com.libraries.jenkins
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class JenkinsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(JenkinsApplication, args)
+	}
+
+}
