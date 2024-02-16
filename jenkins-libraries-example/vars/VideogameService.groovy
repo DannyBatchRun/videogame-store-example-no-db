21ddbee191de7617eb7e9b904fa386ae27d613f6
VintageMachine <danielpeter.mnz@gmail.com>
2024-02-16 11:28:35 +0100

    Adding script once required

--- jenkins-libraries-example/vars/VideogameService.groovy
+++ jenkins-libraries-example/vars/VideogameService.groovy
@@ -1,29 +1,29 @@
 class VideogameService {
-    static void call() {
-        println "Hello from VideogameService!"
+    static void call(script) {
+        script.echo "Hello from VideogameService!"
     }
 
-    static void createJarFile(String PATH) {
-        dir("videogame-store-example-no-db/${PATH}") {
-            sh("mvn -v")
-            sh("mvn clean install")
+    static void createJarFile(script, String PATH) {
+        script.dir("videogame-store-example-no-db/${PATH}") {
+            script.sh("mvn -v")
+            script.sh("mvn clean install")
         }
     }
 
-    static void buildAndPushOnDocker(String PATH, String IMAGE_NAME, String IMAGE_TAG, String passwordEncrypted) {
-        dir("videogame-store-example-no-db/${PATH}") {
-            sh("docker buildx build . -t ${IMAGE_NAME}")
-            sh("docker tag ${IMAGE_NAME} ${params.USERNAME_DOCKERHUB}/${IMAGE_NAME}:${IMAGE_TAG}")
-            useAnsibleVault("${passwordEncrypted}", "decrypt")
-            sh("docker push ${params.USERNAME_DOCKERHUB}/${IMAGE_NAME}:${IMAGE_TAG}")
-            useAnsibleVault("${passwordEncrypted}", "encrypt")
+    static void buildAndPushOnDocker(script, String PATH, String IMAGE_NAME, String IMAGE_TAG, String passwordEncrypted) {
+        script.dir("videogame-store-example-no-db/${PATH}") {
+            script.sh("docker buildx build . -t ${IMAGE_NAME}")
+            script.sh("docker tag ${IMAGE_NAME} ${script.params.USERNAME_DOCKERHUB}/${IMAGE_NAME}:${IMAGE_TAG}")
+            useAnsibleVault(script, "${passwordEncrypted}", "decrypt")
+            script.sh("docker push ${script.params.USERNAME_DOCKERHUB}/${IMAGE_NAME}:${IMAGE_TAG}")
+            useAnsibleVault(script, "${passwordEncrypted}", "encrypt")
         }
     }
 
-    static void useAnsibleVault(String passwordEncrypted, String choice) {
-        dir("/home/daniele/.docker") {
-            sh("( set +x; echo ${passwordEncrypted} > passwordFile )")
-            sh("ansible-vault ${choice} config.json --vault-password-file passwordFile && rm passwordFile")
+    static void useAnsibleVault(script, String passwordEncrypted, String choice) {
+        script.dir("/home/daniele/.docker") {
+            script.sh("( set +x; echo ${passwordEncrypted} > passwordFile )")
+            script.sh("ansible-vault ${choice} config.json --vault-password-file passwordFile && rm passwordFile")
         }
     }
 }
