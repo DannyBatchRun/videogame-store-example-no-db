89530e211da16f9bce4ef3f4a61b9d8c2784a0d2
VintageMachine <danielpeter.mnz@gmail.com>
2024-02-16 10:30:44 +0100

    Added vars folder

---
+++ jenkins-libraries-example/vars/VideogameService.groovy
@@ -0,0 +1,29 @@
+def helloTest() {
+    println "Hello from VideogameService!"
+}
+
+    
+
+def createJarFile(def PATH) {
+    dir("videogame-store-example-no-db/${PATH}") {
+        sh("mvn -v")
+        sh("mvn clean install")
+    }
+}
+
+def buildAndPushOnDocker(def PATH, def IMAGE_NAME, def IMAGE_TAG, def passwordEncrypted) {
+    dir("videogame-store-example-no-db/${PATH}") {
+        sh("docker buildx build . -t ${IMAGE_NAME}")
+        sh("docker tag ${IMAGE_NAME} ${params.USERNAME_DOCKERHUB}/${IMAGE_NAME}:${IMAGE_TAG}")
+        useAnsibleVault("${passwordEncrypted}", "decrypt")
+        sh("docker push ${params.USERNAME_DOCKERHUB}/${IMAGE_NAME}:${IMAGE_TAG}")
+        useAnsibleVault("${passwordEncrypted}", "encrypt")
+    }
+}
+
+def useAnsibleVault(def passwordEncrypted, def choice) {
+    dir("/home/daniele/.docker") {
+        sh("( set +x; echo ${passwordEncrypted} > passwordFile )")
+        sh("ansible-vault ${choice} config.json --vault-password-file passwordFile && rm passwordFile")
+    }
+}
