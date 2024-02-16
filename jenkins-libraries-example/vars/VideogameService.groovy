778fa29e52a4e83b9f2ef1b913203fbdca625448
VintageMachine <danielpeter.mnz@gmail.com>
2024-02-16 11:13:19 +0100

    Another test

--- jenkins-libraries-example/vars/VideogameService.groovy
+++ jenkins-libraries-example/vars/VideogameService.groovy
@@ -1,15 +1,16 @@
-def call() {
+static void call() {
     println "Hello from VideogameService!"
+    return this;
 }
 
-def createJarFile(def PATH) {
+static void createJarFile(def PATH) {
     dir("videogame-store-example-no-db/${PATH}") {
         sh("mvn -v")
         sh("mvn clean install")
     }
 }
 
-def buildAndPushOnDocker(def PATH, def IMAGE_NAME, def IMAGE_TAG, def passwordEncrypted) {
+static void buildAndPushOnDocker(def PATH, def IMAGE_NAME, def IMAGE_TAG, def passwordEncrypted) {
     dir("videogame-store-example-no-db/${PATH}") {
         sh("docker buildx build . -t ${IMAGE_NAME}")
         sh("docker tag ${IMAGE_NAME} ${params.USERNAME_DOCKERHUB}/${IMAGE_NAME}:${IMAGE_TAG}")
@@ -19,7 +20,7 @@ def buildAndPushOnDocker(def PATH, def IMAGE_NAME, def IMAGE_TAG, def passwordEn
     }
 }
 
-def useAnsibleVault(def passwordEncrypted, def choice) {
+static void useAnsibleVault(def passwordEncrypted, def choice) {
     dir("/home/daniele/.docker") {
         sh("( set +x; echo ${passwordEncrypted} > passwordFile )")
         sh("ansible-vault ${choice} config.json --vault-password-file passwordFile && rm passwordFile")
