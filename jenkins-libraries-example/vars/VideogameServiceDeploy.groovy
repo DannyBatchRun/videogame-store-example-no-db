8196acbd8194425f75f24771a0fb498e02f77037
VintageMachine <danielpeter.mnz@gmail.com>
2024-02-21 16:55:41 +0100

    Modified Pipeline and insert a new library VideogameServiceDeploy

---
+++ jenkins-libraries-example/vars/VideogameServiceDeploy.groovy
@@ -0,0 +1,42 @@
+def call() {
+    println "VideogameServiceDeploy initialized"
+    return this;
+}
+
+def getServicePort(def microservice) {
+    def servicePort 
+    switch (microservice) {
+        case "usersubscription":
+            servicePort = "8081"
+            break
+        case "videogameproducts":
+                servicePort = "8100"
+            break
+        case "videogamestore":
+            servicePort = "8080"
+            break
+        default:
+            servicePort = "80"
+    }
+    return servicePort
+}
+
+def pullDockerImage(def deployAll, def imageName, def imageVersion) {
+    if (deployAll) {
+        sh("docker pull index.docker.io/dannybatchrun/usersubscription:${imageVersion}")
+        sh("docker pull index.docker.io/dannybatchrun/videogameproducts:${imageVersion}")
+        sh("docker pull index.docker.io/dannybatchrun/videogamestore:${imageVersion}")
+    } else if (!deployAll) {
+        sh("docker pull index.docker.io/dannybatchrun/${imageName}:${imageVersion}")
+    }
+}
+
+def upgradeHelmDeployment(def imageName, def imageTag, def servicePort) {
+    dir("helm-integration/${imageName}") {
+        sh("sed -i 's/^version: 0.1.0/version: '\"${imageTag}\"'/' Chart.yaml")
+        sh("helm package .")
+        sh("kubectl scale --replicas=0 deployment/${imageName}")
+        sh("helm upgrade ${imageName} . --set image.repository=index.docker.io/dannybatchrun/${imageName},image.tag=${imageTag},image.pullPolicy=Always,service.port=${servicePort},livenessProbe.httpGet.path=/health,livenessProbe.httpGet.port=${servicePort}")
+        sh("kubectl scale --replicas=1 deployment/${imageName}")
+    }
+}
--- jenkins-pipelines/videogamestore-deploy-pipeline-complete.groovy
+++ jenkins-pipelines/videogamestore-deploy-pipeline-complete.groovy
@@ -1,4 +1,8 @@
+@Library('jenkins-library-videogame-store') 
+
 def SERVICE_PORT
+
+def deployService = new VideogameServiceDeploy().call()
 
 pipeline {
     agent any
@@ -8,10 +12,10 @@ pipeline {
         booleanParam(name: 'DEPLOY_ALL', description: "ATTENZIONE! Seleziona questa casella solo se intendi effettuare il deploy di tutti i microservizi. La versione dovrà esistere per tutti i microservizi.")
     }
     stages {
-        stage('Setup Parameters') {
+        stage('Setup Service Port') {
             steps {
                 script {
-                    SERVICE_PORT = params.DEPLOY_ALL ? false : getServicePort("${params.IMAGE_NAME}")
+                    SERVICE_PORT = params.DEPLOY_ALL ? false : deployService.getServicePort("${params.IMAGE_NAME}")
                     //To be removed after merge
                     sh("git checkout helmIntegration")
                 }
@@ -21,7 +25,7 @@ pipeline {
             steps {
                 script {
                     sh("docker version")
-                    pullDockerImage("${params.DEPLOY_ALL}","${IMAGE_NAME}","${IMAGE_VERSION}")
+                    deployService.pullDockerImage("${params.DEPLOY_ALL}","${IMAGE_NAME}","${IMAGE_VERSION}")
                 } 
             }
         }
@@ -29,11 +33,11 @@ pipeline {
             steps {
                 script {
                     if(!params.DEPLOY_ALL) {
-                        upgradeHelmDeployment("${params.IMAGE_NAME}","${params.IMAGE_VERSION}","${SERVICE_PORT}")
+                        deployService.upgradeHelmDeployment("${params.IMAGE_NAME}","${params.IMAGE_VERSION}","${SERVICE_PORT}")
                     } else if (params.DEPLOY_ALL) {
-                        upgradeHelmDeployment("usersubscription","${params.IMAGE_VERSION}","8081")
-                        upgradeHelmDeployment("videogameproducts","${params.IMAGE_VERSION}","8100")
-                        upgradeHelmDeployment("videogamestore","${params.IMAGE_VERSION}","8080")
+                        deployService.upgradeHelmDeployment("usersubscription","${params.IMAGE_VERSION}","8081")
+                        deployService.upgradeHelmDeployment("videogameproducts","${params.IMAGE_VERSION}","8100")
+                        deployService.upgradeHelmDeployment("videogamestore","${params.IMAGE_VERSION}","8080")
                     }
                 } 
             }
@@ -47,46 +51,13 @@ pipeline {
         }
     }
     post {
-        always {
+        success {
+            echo "Pipeline Success"
             cleanWs()
         }
-    }
-}
-
-def getServicePort(def microservice) {
-    def servicePort 
-    switch (microservice) {
-        case "usersubscription":
-            servicePort = "8081"
-            break
-        case "videogameproducts":
-                servicePort = "8100"
-            break
-        case "videogamestore":
-            servicePort = "8080"
-            break
-        default:
-            servicePort = "80"
-    }
-    return servicePort
-}
-
-def pullDockerImage(def deployAll, def imageName, def imageVersion) {
-    if (deployAll) {
-        sh("docker pull index.docker.io/dannybatchrun/usersubscription:${imageVersion}")
-        sh("docker pull index.docker.io/dannybatchrun/videogameproducts:${imageVersion}")
-        sh("docker pull index.docker.io/dannybatchrun/videogamestore:${imageVersion}")
-    } else if (!deployAll) {
-        sh("docker pull index.docker.io/dannybatchrun/${imageName}:${imageVersion}")
-    }
-}
-
-def upgradeHelmDeployment(def imageName, def imageTag, def servicePort) {
-    dir("helm-integration/${imageName}") {
-        sh("sed -i 's/^version: 0.1.0/version: '\"\${imageName}\"'/' Chart.yaml")
-        sh("helm package .")
-        sh("kubectl scale --replicas=0 deployment/${imageName}")
-        sh("helm upgrade ${imageName} . --set image.repository=index.docker.io/dannybatchrun/${imageName},image.tag=${imageTag},image.pullPolicy=Always,service.port=${servicePort},livenessProbe.httpGet.path=/health,livenessProbe.httpGet.port=${servicePort}")
-        sh("kubectl scale --replicas=1 deployment/${imageName}")
+        failure {
+            echo "Pipeline Failure"
+            cleanWs()
+        }
     }
 }
