2dc90d81b01b35769a10f2f2a57b4948e30656ca
Daniele <116575866+DannyBatchRun@users.noreply.github.com>
2024-03-01 13:04:40 +0100

    Update VideogameAutomationService.groovy

--- jenkins-libraries-example/vars/VideogameAutomationService.groovy
+++ jenkins-libraries-example/vars/VideogameAutomationService.groovy
@@ -3,6 +3,14 @@ def call() {
     return this;
 }
 
+
+def installIntoDirectory(def path, def testType) {
+    dir ("${path}/cucumber-auto/${testType}") {
+        sh("npm install --save @cucumber/cucumber axios pactum")
+        echo "Dependencies installed for ${testType}"
+    }
+}
+
 def installDependenciesNodeJs(def microservice) {
     switch("${microservice}") {
         case "usersubscription":
@@ -23,32 +31,38 @@ def installDependenciesNodeJs(def microservice) {
     }
 }
 
-def installIntoDirectory(def path, def testType) {
-    dir ("${path}/cucumber-auto/${testType}") {
-        sh("npm install --save @cucumber/cucumber axios pactum")
-        echo "Dependencies installed for ${testType}"
+def retryForward(def microservice, def servicePort, def podName) {
+    sh("pgrep -f \'kubectl port-forward ${podName}\' | xargs kill")
+    sh("rm ${microservice}output.log || true")
+    sh("nohup kubectl port-forward ${podName} ${servicePort}:${servicePort} > ${microservice}output.log 2>&1 &")
+    sleep 20
+}
+
+def forceForwardIfRequired(def microservice, def servicePort, def podName) {
+    def isNotForwarded = true
+    while (isNotForwarded) {
+        def responseCode = sh(script: "curl -s -o /dev/null -w \"%{http_code}\" http://localhost:${servicePort}/health || true", returnStdout: true).trim()
+        if (responseCode != '200') {
+            println "Service ${microservice} is not responding. Forwarding port again and cleaning up old forward..."
+            retryForward("${microservice}","${servicePort}","${podName}")
+        } else {
+            println "Service ${microservice} is running."
+            isNotForwarded = false
+        }
     }
 }
 
-def forwardKubernetesPort(def microservice, def choice) {
-    def servicePort
-    switch("${microservice}") {
-        case "usersubscription":
-            servicePort = "8081"
-        break
-        case "videogameproducts":
-            servicePort = "8100"
-        break
-        case "videogamestore":
-            servicePort = "8080"
-        break
-    }
+def forwardKubernetesPort(def microservice, def servicePort, def choice) {
     def podName = sh(script: "kubectl get pods -l \"app.kubernetes.io/instance=${microservice}\" -o jsonpath='{.items[0].metadata.name}'", returnStdout: true).trim()
     echo "Pod Name ${microservice}: ${podName}"
     if(choice.equals("open")) {
-        sh("nohup kubectl port-forward ${podName} ${servicePort}:${servicePort} > output.log 2>&1 &")
-    } else if(choice.equals("close")) {
-        sh("pgrep -f 'kubectl port-forward ${podName}' | xargs kill")
+        sh("nohup kubectl port-forward ${podName} ${servicePort}:${servicePort} > ${microservice}output.log 2>&1 &")
+        echo "Waiting for a few seconds before continue."
+        sleep 20
+        echo "Checking if the pod is in running..."
+        forceForwardIfRequired("${microservice}","${servicePort}","${podName}")
+    } else if (choice.equals("close")) {
+        sh("pgrep -f \'kubectl port-forward ${podName}\' | xargs kill")
     }
 }
 
