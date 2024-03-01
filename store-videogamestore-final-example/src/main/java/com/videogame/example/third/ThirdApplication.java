97ded1fb3365de04becba4484e3641bffae0974a
Daniele <116575866+DannyBatchRun@users.noreply.github.com>
2024-03-01 10:47:34 +0100

    Update ThirdApplication.java

--- store-videogamestore-final-example/src/main/java/com/videogame/example/third/ThirdApplication.java
+++ store-videogamestore-final-example/src/main/java/com/videogame/example/third/ThirdApplication.java
@@ -9,5 +9,8 @@ public class ThirdApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ThirdApplication.class, args);
 	}
-
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder){
+        return restTemplateBuilder.setConnectTimeout(Duration.ofSeconds(5)).setReadTimeout(Duration.ofSeconds(5)).build();
+    }
 }
