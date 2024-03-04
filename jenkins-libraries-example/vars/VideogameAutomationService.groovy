cecd94983070d86ecb2a9ff1aa35ed136597dbfe
Daniele <116575866+DannyBatchRun@users.noreply.github.com>
2024-03-04 11:47:04 +0100

    Update VideogameAutomationService.groovy

--- jenkins-libraries-example/vars/VideogameAutomationService.groovy
+++ jenkins-libraries-example/vars/VideogameAutomationService.groovy
@@ -32,7 +32,7 @@ def installDependenciesNodeJs(def microservice) {
 }
 
 def retryForward(def microservice, def servicePort, def podName) {
-    sh('pkill -f "kubectl port-forward $${podName}"')
+    sh("pkill -f 'kubectl port-forward ${podName}'")
     sh("rm ${microservice}output.log || true")
     sh("nohup kubectl port-forward ${podName} ${servicePort}:${servicePort} > ${microservice}output.log 2>&1 &")
     sleep 20
@@ -62,7 +62,7 @@ def forwardKubernetesPort(def microservice, def servicePort, def choice) {
         echo "Checking if the pod is in running..."
         forceForwardIfRequired("${microservice}","${servicePort}","${podName}")
     } else if (choice.equals("close")) {
-        sh('pkill -f "kubectl port-forward $${podName}"')
+        sh("pkill -f 'kubectl port-forward ${podName}'")
    }
 }
 
